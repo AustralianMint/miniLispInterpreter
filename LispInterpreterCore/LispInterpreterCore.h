caf1b5c05cce32c9e0003ca0efd62248c47242d8
AustralianMint <thomas.frey@code.berlin>
2024-11-06 00:39:51 +0100

    refactor: add public wrapper to interpreter class

---
+++ LispInterpreterCore/LispInterpreterCore.h
@@ -0,0 +1,18 @@
+//
+//  LispInterpreterCore.h
+//  LispInterpreterCore
+//
+//  Created by Thomas Frey on 05.11.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LispInterpreterCore.
+FOUNDATION_EXPORT double LispInterpreterCoreVersionNumber;
+
+//! Project version string for LispInterpreterCore.
+FOUNDATION_EXPORT const unsigned char LispInterpreterCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LispInterpreterCore/PublicHeader.h>
+
+
