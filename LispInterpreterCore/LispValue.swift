acd9ea9fcc69ee16b2e027ddd421fa5fe65b7dfb
AustralianMint <thomas.frey@code.berlin>
2024-11-06 18:04:28 +0100

    add function to define comparison of values

--- LispInterpreterCore/LispValue.swift
+++ LispInterpreterCore/LispValue.swift
@@ -6,10 +6,29 @@
 
 // Needed to specify which values the interpreter understands
 
-public enum LispValue {
+public enum LispValue: Equatable {
     case symbol(String)
     case number(Double)
     case list([LispValue])
     case function((([LispValue]) throws -> LispValue))
     case null
+    
+    // Needed to conform to the enum's Equatable protocol
+    // Will define how two values will be compared for equality
+    public static func == (lhs: LispValue, rhs: LispValue) -> Bool {
+        switch (lhs, rhs) {
+        case (.number(let l), .number(let r)):
+            return l == r
+        case (.symbol(let l), .symbol(let r)):
+            return l == r
+        case (.list(let l), .list(let r)):
+            return l == r
+        case (.null, .null):
+            return true
+        case (.function, .function):
+            return false
+        default:
+            return false
+        }
+    }
 }
